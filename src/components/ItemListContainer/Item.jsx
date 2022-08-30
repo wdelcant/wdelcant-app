28ebade8eb658932ec842cd89260a6d36822d4f3
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-08-30 19:57:40 -0400

    se repara favorito

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ItemCount from '../ItemCount/ItemCount';
+import { BsHeartFill } from 'react-icons/bs';
+
+
+
+
+
 
 
 const Item = ({id, img, title, price, description, stock}) => {
+
+    const handleClick = event => {
+        event.currentTarget.classList.toggle('active');
+    };
+
     return (
             <div className="item-list-container__item" id="item">
                 <div className="item-list-container__item__image"><img src={img} alt={title}/></div>
                 <h2 className="item-list-container__item__title">{title}</h2>
                 <p className="item-list-container__item__price">${price}</p>
                 <p className="item-list-container__item__description">{description}</p>
-                <span className="item-list-container__item__icon"><img src="assets/images/heart.svg" alt="" /></span>
+                <span className=" item-list-container__item__icon favme" onClick={handleClick}><BsHeartFill/></span>
                 <ItemCount stock={stock}/>
             </div>  
     )
