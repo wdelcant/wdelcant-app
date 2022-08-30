8e4e7f5a32d9a1fcd5ded427f13341b620e83876
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-08-29 21:01:13 -0400

    fix cards position

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -5,7 +5,7 @@ import ItemCount from '../ItemCount/ItemCount';
 const Item = ({id, img, title, price, description, stock}) => {
     return (
             <div className="item-list-container__item" id="item">
-                <img className="item-list-container__item__image" src={img} alt={title}/>
+                <div className="item-list-container__item__image"><img src={img} alt={title}/></div>
                 <h2 className="item-list-container__item__title">{title}</h2>
                 <p className="item-list-container__item__price">${price}</p>
                 <p className="item-list-container__item__description">{description}</p>
