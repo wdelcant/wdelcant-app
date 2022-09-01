1e64ae5890e2cf2cfea22f5c15aa2ddc0cc75a4e
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-09-01 00:46:18 -0400

    Se estiliza la web en general

---
+++ src/components/Footer/FooterSecondary.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Footer.scss'
+import { BsHeadset, BsTruck, BsPatchCheck } from 'react-icons/bs';
+
+const FooterSecunday = () => {
+  return (
+  <section className="section-footer">
+    <div className="footer__secondary">
+        <div className="footer__secondary--contact">
+          <BsHeadset className="footer__secondary--icon"/>
+          <p>Soporte Técnico</p>
+        </div>
+
+        <div className="footer__secondary--warranty">
+          <BsPatchCheck className="footer__secondary--icon"/>
+          <p>Garantía</p>
+        </div>
+
+        <div className="footer__secondary--shipments">
+          <BsTruck className="footer__secondary--icon"/>
+          <img className="footer__secondary--img" src="assets/images/logo.svg" alt="" />
+          <p>Envíos</p>
+        </div>
+    </div>
+  </section>
+  )
+}
+
+export default FooterSecunday
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,43 +18,49 @@ const NavBar = () => {
 
             <li className="menu__item menu__item--show">
                 <a href="#category" className="menu__link">Categorías <img src="assets/images/arrow.svg" className="menu__arrow" aria-label="imagen flecha" /></a>
-            <ul className="menu__nesting">
-                <li className="menu__inside">
-                    <a href="#processor" className="menu__link menu__link--inside">Procesadores</a>
-                </li>
-                <li className="menu__inside">
-                    <a href="#motherboard" className="menu__link menu__link--inside">Placas Madres</a>
-                </li>
-                <li className="menu__inside">
-                    <a href="#memory" className="menu__link menu__link--inside">Memorias</a>
-                </li>
-                <li className="menu__inside">
-                    <a href="#graphics" className="menu__link menu__link--inside">Tarjetas Gráficas</a>
-                </li>
-                <li className="menu__inside">
-                    <a href="#storage" className="menu__link menu__link--inside">Almacenamiento</a>
-                </li>
-                <li className="menu__inside">
-                    <a href="#cases" className="menu__link menu__link--inside">Gabinetes</a>
-                </li>
-                <li className="menu__inside">
-                <a href="#powersupply" className="menu__link menu__link--inside">Fuentes de poder</a>
-                </li>
-                
-            </ul>
+            
+                <ul className="menu__nesting">
+                    <li className="menu__inside">
+                        <a href="#processor" className="menu__link menu__link--inside">Procesadores</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#motherboard" className="menu__link menu__link--inside">Placas Madres</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#memory" className="menu__link menu__link--inside">Memorias</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#graphics" className="menu__link menu__link--inside">Tarjetas Gráficas</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#storage" className="menu__link menu__link--inside">Almacenamiento</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#cases" className="menu__link menu__link--inside">Gabinetes</a>
+                    </li>
+                    <li className="menu__inside">
+                        <a href="#powersupply" className="menu__link menu__link--inside">Fuentes de poder</a>
+                    </li>
+                </ul>
             </li>
+
             <li className="menu__item">
                 <a href="#offers" className="menu__link">Ofertas</a>
             </li>
+
             <li className="menu__item">
                 <a href="#services" className="menu__link">Servicios</a>
             </li>
+
             <li className="menu__item">
                 <a href="#contact" className="menu__link">Contacto</a>
             </li>
+
         </ul>
+
         <Form />
-        <div className="menu__users">
+
+        <div className="menu__users menu__links">
             <li><button className="menu__users--login" href="#">Iniciar</button></li>
             <li><button className="menu__users--register" href="#">Registrar</button></li>
         </div>
