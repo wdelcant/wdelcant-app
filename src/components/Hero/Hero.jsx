15a09677a134aefb6034db714c9e6c4ad8bad737
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-09-05 00:41:19 -0400

    Se añade Hero con slide

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Hero.scss";
 
-const Next = () => {
-
-}
-
-const Prev = () => {
-
-}
-
-
-
 const Hero = () => {
+  const slide = useRef(null);
+
+  const Next = () => {
+    if (slide.current.children.length > 0) {
+      // Comprobamos que el slidershow tenga elementos
+      const firstElement = slide.current.children[0];
+
+      // Establecemos la animación
+      slide.current.style.transition = `600ms ease-out all`;
+
+      // Obtenemos el tamaño del primer elemento
+      const size = slide.current.children[0].offsetWidth;
+
+      // Movemos el slider
+      slide.current.style.transform = `translateX(-${size}px)`;
+
+      // Cuando la animación termine
+      const transition = () => {
+        slide.current.style.transition = `none`;
+        slide.current.style.transform = `translateX(0)`;
+        slide.current.appendChild(firstElement);
+        slide.current.removeEventListener("transitionend", transition);
+      };
+
+      // Añadimos el evento
+      slide.current.addEventListener("transitionend", transition);
+    }
+  };
+
+  const Prev = () => {
+    if (slide.current.children.length > 0) {
+      // Comprobamos que el slidershow tenga elementos
+      const index = slide.current.children.length - 1;
+      const lastElement = slide.current.children[index];
+      slide.current.insertBefore(lastElement, slide.current.firstChild);
+
+      slide.current.style.transition = `none`;
+      const size = slide.current.children[0].offsetWidth;
+      slide.current.style.transform = `translateX(-${size}px)`;
+
+      setTimeout(() => {
+        slide.current.style.transition = `600ms ease-out all`;
+        slide.current.style.transform = `translateX(0)`;
+      }, 30);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      Next();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="container-slider">
-      <div className="slider" id="slider">
+      <div className="slider" ref={slide}>
         <div className="slider__section">
           <img
             src="assets/images/banner/image1.png"
@@ -45,10 +89,18 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className="slider__btn slider__btn--right" id="btn-right" onClick={Next}>
+      <div
+        className="slider__btn slider__btn--right"
+        id="btn-right"
+        onClick={Next}
+      >
         &#62;
       </div>
-      <div className="slider__btn slider__btn--left" id="btn-left" onClick={Prev}>
+      <div
+        className="slider__btn slider__btn--left"
+        id="btn-left"
+        onClick={Prev}
+      >
         &#60;
       </div>
     </div>
