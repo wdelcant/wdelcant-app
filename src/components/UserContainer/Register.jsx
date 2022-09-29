ede38263657087e976faf3ffd25a2527b31b49e8
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-09-28 21:04:38 -0300

    Se agrega base de datos de usuarios registrados

--- src/components/UserContainer/Register.jsx
+++ src/components/UserContainer/Register.jsx
@@ -4,18 +4,35 @@ import { useAuth } from "../../context/AuthContext";
 import ReCAPTCHA from "react-google-recaptcha";
 import Swal from "sweetalert2";
 import "./Register.scss";
+import db from "../../utils/firebaseConfig";
+import { collection } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
 
 export function Register() {
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+
   const { signUp } = useAuth();
 
   const [user, setUser] = useState({
+    fullName: "",
     email: "",
+    repeatEmail: "",
+    phone: "",
     password: "",
     password2: "",
   });
+  const { fullName, email, repeatEmail, phone, password, password2 } = user;
 
-  const [error, setError] = useState();
-  const navigate = useNavigate();
+  const addUserDb = async (data) => {
+    try {
+      const col = collection(db, "users");
+      const users = await addDoc(col, data);
+      setUser(users.uid);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const alertRegister = () => {
     const Toast = Swal.mixin({
@@ -40,11 +57,15 @@ export function Register() {
     e.preventDefault();
     setError("");
     if (user.password !== user.password2) {
-      setError("Passwords do not match");
+      setError("Las contraseñas no coinciden");
+    }
+    if (user.email !== user.repeatEmail) {
+      setError("Los emails no coinciden");
     } else {
       try {
         if (recaptchaRef.current.getValue()) {
           await signUp(user.email, user.password);
+          addUserDb(user);
           alertRegister();
           navigate("/");
         } else {
@@ -52,13 +73,13 @@ export function Register() {
         }
       } catch (error) {
         if (error.code === "auth/email-already-in-use") {
-          setError("Email already in use");
+          setError("El email ya está registrado");
         }
         if (error.code === "auth/weak-password") {
-          setError("Password should be at least 6 characters");
+          setError("La contraseña debe tener al menos 6 caracteres");
         }
         if (error.code === "auth/invalid-email") {
-          setError("Invalid email");
+          setError("El email no es válido");
         }
       }
     }
@@ -77,14 +98,48 @@ export function Register() {
         <h1 className="register__content--title">Registro</h1>
         {error && <small>{error}</small>}
         <form className="register__content--form" onSubmit={handleSubmit}>
+          <label htmlFor="fullName">Nombre</label>
+          <input
+            className="register__content--form--input"
+            type="text"
+            name="fullName"
+            id="fullName"
+            value={fullName}
+            placeholder="Nombre Apellido"
+            onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+          />
+
           <label htmlFor="email">Correo</label>
           <input
             className="register__content--form--input"
             type="email"
             name="email"
             id="email"
+            value={email}
             placeholder="correo@mail.com"
             onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+
+          <label htmlFor="repeatEmail">Confirmar Correo</label>
+          <input
+            className="register__content--form--input"
+            type="email"
+            name="repeatEmail"
+            id="repeatEmail"
+            value={repeatEmail}
+            placeholder="correo@mail.com"
+            onChange={(e) => setUser({ ...user, repeatEmail: e.target.value })}
+          />
+
+          <label htmlFor="phone">Teléfono</label>
+          <input
+            className="register__content--form--input"
+            type="tel"
+            name="phone"
+            id="phone"
+            value={phone}
+            placeholder="56912345678"
+            onChange={(e) => setUser({ ...user, phone: e.target.value })}
           />
 
           <label htmlFor="password">Contraseña</label>
@@ -93,6 +148,7 @@ export function Register() {
             type="password"
             name="password"
             id="password"
+            value={password}
             placeholder="********"
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
@@ -103,6 +159,7 @@ export function Register() {
             type="password"
             name="password2"
             id="password2"
+            value={password2}
             placeholder="********"
             onChange={(e) => setUser({ ...user, password2: e.target.value })}
           />
