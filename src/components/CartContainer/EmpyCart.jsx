9740115d952f68a92d441273285340cae1ac416c
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-09-30 17:35:39 -0300

    fixes

--- src/components/CartContainer/EmpyCart.jsx
+++ src/components/CartContainer/EmpyCart.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './EmpyCart.scss';
 
 // Devuelve un div con una imagen, un título y un enlace a la página de inicio.
 const EmpyCart = () => {
   return (
     <div className="cart-container">
+      <h3 className="cart__container--empy--title">Su cesta está vacía. :( </h3>
       <img src="/assets/images/alert-circle.svg" alt="cart" />
-      <h3 className="cart__title">No hay productos agregados al carro :( </h3>
-      <Link to="/" className="">
+      <p className="cart__container--empy--text">
+        ¡Llénalo con nuestros mejores productos!
+      </p>
+      <Link to="/" className="cart__container--empy--button">
         Comprar productos
       </Link>
     </div>
